8c2f887dabb33530225e30a52e4d422a718b3490
Jan Kliszcz <j.kliszcz@tim.pl>
2022-07-10 09:59:20 +0200

    Added log creation feature to script

--- skrypt.sh
+++ skrypt.sh
@@ -9,3 +9,14 @@ if [ $1 == '--date' ]
 then
 	date
 fi
+
+if [ $1 == '--logs' ]
+then
+	scriptName="${0##*/}"
+	for i in {1..100}
+	do
+		fileName="log$i"
+		date="$(date)"
+		echo "$fileName $scriptName $date" >> $fileName
+	done
+fi
